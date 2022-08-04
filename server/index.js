de906836fddb591fb06408eb008c5a9371c26f94
ninhpm95 <ninhpm95@gmail.com>
2022-08-04 19:30:17 +0900

    Add semicolons

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const WebSocket = require('ws');
-const R = require('ramda');
 const { v4: uuidv4 } = require('uuid');
 
 const SOCKET_PORT = 3002;
@@ -16,17 +15,21 @@ const {
 
   CHANGE_USERNAME,
   DELETE_LAST_MESSAGE
-} = require('./const/SocketActionTypes')
+} = require('./const/SocketActionTypes');
 
-const server = new WebSocket.Server({ port: SOCKET_PORT })
+const server = new WebSocket.Server({ port: SOCKET_PORT });
 
-const users = {}
+const users = {};
 
-const messages = {}
+const messages = {};
 
-const findLastMessageByUserId = (messagesArray, userId) => (
-  R.findLast(R.propEq('userId', userId))(messagesArray)
-)
+const findLastMessageByUserId = (messageArray, userId) => {
+  for (let i = messageArray.length - 1; i >= 0; i--) {
+    if (messageArray[i].userId === userId) {
+      return messageArray[i];
+    }
+  }
+};
 
 server.on('connection', ws => {
   const CurrentId = uuidv4();
@@ -34,7 +37,7 @@ server.on('connection', ws => {
   users[CurrentId] = {
     id: CurrentId,
     name: `User-${CurrentId.substring(0, 5)}`,
-  }
+  };
 
   broadcastToSelf({
     type: USER_CONNECTED,
@@ -43,41 +46,39 @@ server.on('connection', ws => {
       users,
       messages,
     },
-  }, ws)
+  }, ws);
 
   broadcastToOthers({
     type: USER_LIST_UPDATED,
     payload: {
       users,
     },
-  }, ws)
+  }, ws);
 
-  console.log('User Connected')
-  console.log('User:', users[CurrentId])
-  console.log('Users:', users)
+  console.log('User connected');
+  console.log('User: ', users[CurrentId]);
 
   ws.on('message', message => {
-    const action = JSON.parse(message)
+    const action = JSON.parse(message);
 
     switch (action.type) {
       case USER_TYPING: {
         users[action.payload.userId] = {
           ...users[action.payload.userId],
           typing: action.payload.typing,
-        }
+        };
 
         broadcastToOthers({
           type: USER_LIST_UPDATED,
           payload: {
             users,
           },
-        })
+        });
 
-        console.log('User Typing')
-        console.log('User:', users[action.payload.userId])
-        console.log('Users:', users)
+        console.log('User is typing');
+        console.log('User: ', users[action.payload.userId]);
 
-        break
+        break;
       }
 
       case MESSAGE_SENT: {
@@ -90,7 +91,7 @@ server.on('connection', ws => {
           text: action.payload.text,
           time: action.payload.time,
           userId: action.payload.userId,
-        }
+        };
 
         broadcastToSelf({
           type: MESSAGE_RECEIVED,
@@ -98,102 +99,98 @@ server.on('connection', ws => {
             message: messages[NewMessageId],
             messages,
           },
-        }, ws)
+        }, ws);
 
         broadcastToOthers({
           type: MESSAGE_LIST_UPDATED,
           payload: {
             messages,
           },
-        }, ws)
+        }, ws);
 
-        console.log('Message Received')
-        console.log('Message:', messages[NewMessageId])
-        console.log('Messages:', messages)
+        console.log('Message received');
+        console.log('Message: ', messages[NewMessageId]);
 
-        break
+        break;
       }
 
       case CHANGE_USERNAME: {
         users[action.payload.userId] = {
           ...users[action.payload.userId],
           name: action.payload.userName,
-        }
+        };
 
         broadcastToAll({
           type: USER_LIST_UPDATED,
           payload: {
             users,
           },
-        })
+        });
 
-        console.log('User Name Updated')
-        console.log('User:', users[action.payload.userId])
-        console.log('Users:', users)
+        console.log('User name updated');
+        console.log('User: ', users[action.payload.userId]);
 
-        break
+        break;
       }
 
       case DELETE_LAST_MESSAGE: {
-        const messagesArray = Object.keys(messages).map(key => messages[key])
-        const lastMessage = findLastMessageByUserId(messagesArray, action.payload.userId)
+        const messageArray = Object.keys(messages).map(key => messages[key]);
+        const lastMessage = findLastMessageByUserId(messageArray, action.payload.userId);
 
         if (lastMessage) {
-          delete messages[lastMessage.id]
+          delete messages[lastMessage.id];
 
           broadcastToAll({
             type: MESSAGE_LIST_UPDATED,
             payload: {
               messages,
             },
-          })
+          });
 
-          console.log('Message Removed')
-          console.log('Message:', lastMessage)
-          console.log('Messages:', messages)
+          console.log('Message removed');
+          console.log('Message: ', lastMessage);
         }
 
-        break
+        break;
       }
 
       default:
-        console.log('Unknown action:', action)
+        console.log('Unknown action: ', action);
     }
   })
 
   ws.on('close', () => {
-    delete users[CurrentId]
+    delete users[CurrentId];
 
     broadcastToOthers({
       type: USER_LIST_UPDATED,
       payload: {
         users,
       },
-    }, ws)
+    }, ws);
 
-    console.log('User Disconnected')
-    console.log('Users:', users)
+    console.log('1 user disconnected');
   })
 })
 
 const broadcastToSelf = (action, ws) => {
-  ws.send(JSON.stringify(action))
-}
+  ws.send(JSON.stringify(action));
+};
 
 const broadcastToOthers = (action, ws) => {
   server.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN && client !== ws) {
-      client.send(JSON.stringify(action))
+      client.send(JSON.stringify(action));
     }
   })
-}
+};
 
 const broadcastToAll = action => {
   server.clients.forEach(client => {
     if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify(action))
+      client.send(JSON.stringify(action));
     }
   })
-}
+};
 
-console.log(`Socket server running on port ${SOCKET_PORT}`)
+console.log(`Socket server running on port ${SOCKET_PORT}`);
